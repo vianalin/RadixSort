b101a6e878a4d4021f24e59b55edc969da815702
vianalin <vlin21@stuy.edu>
2021-01-13 21:27:26 -0500

    nth

--- Radix.java
+++ Radix.java
@@ -1,7 +1,15 @@
 public class Radix {
-	public static int nth(int n, int col);
 
-	public static int length(int n);
+	//1: get nth digit of an int, where 0 is the ones column, 1 is the tens column etc.
+	//123: 2 = hundreds, 1 = tens, 0 = ones
+	public static int nth(int n, int col) {
+		int divisor = (int) Math.pow(10, col);
+		return ((int) n / divisor) % 10;
+	}
 
-	public static void merge(MyLinkedList original, MyLinkedList[] buckets);
+
+
+	//public static int length(int n);
+
+	//public static void merge(MyLinkedList original, MyLinkedList[] buckets);
 }
